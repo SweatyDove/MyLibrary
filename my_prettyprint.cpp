5a7d084a80f74c6720f53b76e6ab7d384c39d672
Alexey Perekhin <aperexin@gmail.com>
2025-03-19 13:21:02 +0300

    [PrettyPrint]: started to write class for pretty output of different messages

--- my_prettyprint.cpp
+++ my_prettyprint.cpp
@@ -1,6 +1,191 @@
 #include "my_prettyprint.h"
 
-PrettyPrint::PrettyPrint()
-{
 
+
+
+//==================================================================================================
+//          TYPE:   Constructor
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+my::PrettyPrint::PrettyPrint()
+{
+    // Nothing to do
 }
+
+
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   Set displayed information levels.
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+void my::PrettyPrint::displayLevel(bool debug, bool info, bool warning, bool error)
+{
+    mb_level[static_cast<int>(Level::DEBUG)] = debug;
+    mb_level[static_cast<int>(Level::INFO)] = info;
+    mb_level[static_cast<int>(Level::WARN)] = warning;
+    mb_level[static_cast<int>(Level::ERROR)] = error;
+}
+
+
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   Is it necessary to display the time?
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   Add a selection of time's format.
+//==================================================================================================
+void my::PrettyPrint::displayTime(bool b)
+{
+    mb_timeDisplay = b;
+}
+
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   Setting the display of the name of the calling function in the output.
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+void my::PrettyPrint::displayFuncName(bool b)
+{
+    mb_funcNameDisplay = b;
+}
+
+
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   Form the base message for different output levels
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+//void my::PrettyPrint::formMessage()
+//{
+
+//}
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+void my::PrettyPrint::printTime()
+{
+    std::time_t timeStamp;
+
+    int buffSize {32};
+    char timeBuff[buffSize];
+
+    std::memset(timeBuff, '\0', buffSize);
+
+    if (mb_timeDisplay == true) {
+        // ## Copy current time value into buffer
+        std::time(&timeStamp);
+        std::strncpy(timeBuff, std::ctime(&timeStamp), buffSize);
+
+        // ## Delete/replace last new-line character(s) with '\0'
+        for (int ii {buffSize - 1}; ii > 0; --ii) {
+            if (timeBuff[ii] == '\0') {
+                continue;
+            }
+            else if (timeBuff[ii] == '\n') {
+                timeBuff[ii] = '\0';
+            }
+            else {
+                break;
+            }
+        }
+
+        // ## Display time
+        std::cout << '[' << timeBuff << ']';
+        printSeparator();
+
+    }
+    else {} // Nothing to do
+}
+
+
+
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+//void my::PrettyPrint::printFuncName()
+//{
+//    std::
+
+//}
+
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+void my::PrettyPrint::setSeparator(const char* sep)
+{
+    mb_separator = sep;
+}
+
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+void my::PrettyPrint::printSeparator()
+{
+    std::cout << mb_separator;
+}
+
+
+
+
+//==================================================================================================
+//          TYPE:   Method
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+void my::PrettyPrint::debug(char* line)
+{
+    if (mb_level[static_cast<int>(Level::DEBUG)] == true) {
+        printTime();
+        std::cout << "[DEBUG]=> " << line << std::endl;
+    }
+    else {
+        // Nothing to do
+    }
+}
+
+
+
+
--- my_prettyprint.h
+++ my_prettyprint.h
@@ -1,11 +1,80 @@
-#ifndef PRETTYPRINT_H
-#define PRETTYPRINT_H
+#ifndef MY_PRETTYPRINT_H
+#define MY_PRETTYPRINT_H
+
+#include <iostream>
+#include <array>
+#include <ctime>
+#include <cstring>                  // For: strncpy(), memset()
+
+namespace my {
 
 
-class PrettyPrint
-{
+/*
+ * ПЛАН:
+ *
+ * -- Функция должна иметь возможность отображать:
+ *      1) Время сообщения
+ *      2) Тип сообщения (INFO, DEBUG, ERROR, WARNING, FATAL/CRITICAL)
+ *      3) Функцию, которая сообщение вызывает
+ *      4) Кастомный вид сообщения, который формируется заранее (всякие отступы и т.п.)
+ *      5) Среду вывода сообщения (файл, stdout/stderr, сокет)
+ *
+ * -- Так же нужно уметь работать со списком аргументов переменной длинны. Или как-то использовать
+ *      стандартный функционал (sprintf() например)
+ *
+ */
+
+
+
+//==================================================================================================
+//          TYPE:   Class
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+// COMMENTS/BUGS:   ........
+//==================================================================================================
+class PrettyPrint {
 public:
+
+    // # Levels of output
+    enum class Level {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+
+        TOTAL
+    };
+    std::array<bool, static_cast<int>(Level::TOTAL)> mb_level {true, true, true, true};
+
+    bool mb_timeDisplay {true};
+    bool mb_funcNameDisplay {true};
+    const char* mb_separator {"::"};
+
+
+
+
     PrettyPrint();
+
+
+    void displayLevel(bool debug, bool info, bool warning, bool error);
+    void displayTime(bool);
+    void displayFuncName(bool);
+
+    void setSeparator(const char* sep);
+    void printSeparator();
+
+
+
+    void printTime();
+    void printFuncName();
+
+//    void formMessage(char* line);
+    void debug(char* line);
 };
 
-#endif // PRETTYPRINT_H
+
+}
+
+
+#endif // MY_PRETTYPRINT_H
