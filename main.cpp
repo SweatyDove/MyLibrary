216766208520268a5355a84086d91531d7d3d25c
Alexey Perekhin <aperexin@gmail.com>
2025-03-25 18:20:31 +0300

    refactor(PrettyPrint): discard unnecessary files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,76 @@
-#include <iostream>
-#include "my_prettyprint.h"
 
 
-#include <execinfo.h>
-#include <cstdio>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "main.h"
+#include "attribute.h"
+
+
+
+/*
+ * QUESTIONS:
+ *
+ * 1) Order of evaluation of 3-d part (incrementing) of the for-loop-statement? (Could begin from
+ *      here: https://en.cppreference.com/w/cpp/language/eval_order
+ */
+
+
+class Base {
+public:
+    int* mb_data;
+    enum class Name {
+        HEALTH,
+        STAMINA,
+        MANA,
+
+        TOTAL
+    };
+
+
+//    inline static const ArrayClass<my::String,  static_cast<unsigned int>(Name::TOTAL)> mb_strArray = {
+//        "Hello",
+//        "Beautifull",
+//        "World!"
+//    };
+
+    Base()
+    {
+        std::cout << "[DEBUG]: Base's default ctor has been called" << std::endl;
+    }
+
+    Base(int a) :
+        mb_data {new int {a} }
+    {
+        std::cout << "[DEBUG]: Base ctor has been called: allocate memory for mb_data" << std::endl;
+    }
+
+    Base& operator=(const Base& base)
+    {
+        std::cout << "[DEBUG]: <Base>'s copy assignment operator has been called" << std::endl;
+        this->mb_data = base.mb_data;
+        return *this;
+    }
+
+    ~Base()
+    {
+        std::cout << "[DEBUG]: Base dtor has been called: delete mb_data allocation" << std::endl;
+        delete mb_data;
+//        mb_data = nullptr;
+    }
+
+//    static const my::String& getStringName(Base::Name name)
+//    {
+//        return mb_strArray[static_cast<unsigned int>(name)];
+//    }
+
+
+
+};
+
+std::ostream& operator<<(std::ostream& out, const Base& base)
+{
+    out << base.mb_data;
+    return out;
+}
+
 
 
 
@@ -14,21 +78,74 @@
 
 int main()
 {
-    int a {759};
-    char* cStr {"World"};
-    char* testString = "Others who use this device won’t see your activity, so you can browse more privately. This won't change how data is collected by websites you visit and the services they use, including Google. Downloads, bookmarks and reading list items will be saved.";
+//    Base a {5};
+//    Base b {7};
+//    int ii {0};
 
-    my::PrettyPrint prettyPrint;
-    prettyPrint.setLevels({false, true, true, false});
+    // Тут происходит две вещи. Как я понимаю, сперва создаётся пустой массив <my::Array>, каждый
+    // элемент которого создаётся при помощи дефолтного конструктора (здесь Base()). Затем происходит
+    // копирование a и b в элементы массива.
+    // Единственное, что при вызове деструктора массива, почему-то вызывается деструктор только для
+    // одного элемента <Base>...
+    my::Array<my::String, 2> temp {
+        "Hello",
+        "World"
+    };
+
+    ArrayClass<StringClass, 3> foo {
+        "Hello",
+        "my",
+        "world!"
+    };
+
+
+    /*
+     * А почему он создается ещё на этапе компиляции?
+     * Тут вроде бы всё нормально.
+     * 1 -- Сперва конструируются временные объекты <Base>, содержащие 5 и 7
+     * соответственно (сами "результаты" выражений (5) и (7) имеют тип int и являются rvalue).
+     * 2 -- Затем сам массив bar конструирует два "свободных" места <Base> под вычисленные элементы -
+     * используя для этого дефолтный конструктор <Base>.
+     * 3 -- Ну и на третем шаге происходит копирование временных объектов в объекты из массивы.
+     * 4 -- Потом временные обьекты уничтожаются.
+     */
+//    my::Array<Base, 2> bar {5, 7};
 
 
 
-    prettyPrint.debug("%s", testString);
-    prettyPrint.info("%s", testString);
-    prettyPrint.warn("%s", testString);
-    prettyPrint.error("%s", testString);
 
+
+//    Attribute attr_1 = Attribute(Attr::Name::DODGE_CHANCE, Attribute::Type::RATING, -1, -1, -1);
+//    Attribute attr_2 {Attr::Name::CRIT_CHANCE, Attribute::Type::RATING, -1, -1, -1};
+//    Attribute attr_3 {Attribute(Attr::Name::ESCAPE_CHANCE, Attribute::Type::RATING, 66, 66, 66)};
+
+//    ArrayClass<Attribute, 3> mb_ratings = {attr_1, attr_2, attr_3};
+//    ArrayClass<Attribute, 3> mb_ratings = {
+//        {
+//            {Attr::Name::DODGE_CHANCE, Attribute::Type::RATING, -1, -1, -1},
+//            {Attr::Name::CRIT_CHANCE, Attribute::Type::RATING, -1, -1, -1},
+//            {Attr::Name::ESCAPE_CHANCE, Attribute::Type::RATING, 66, 66, 66}
+//        }
+//    };
+//    std::array<Attribute, 1> mb_ratingsClassic {attr_1};
+
+    /*
+     * По идее, выбор семантики (копирование или перемещение), зависит не от массива, а от типа
+     * элементов, которые находятся внутри него. То есть когда arrB[0] = arrA[0] - это оператор
+     * присваивания, относящийся НЕПОСРЕДСТВЕННО к элементам массива - и именно эти элементы
+     * ответственны за выбор семантики. А пользователь массива? МОжет ли он указать, что элементы
+     * нужно перемещать, а не копировать (и наоборот)?
+     */
 
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
