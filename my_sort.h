baafb8a06d4046641fc2e34f255a9d1dedb64137
Alexey Perekhin <aperexin@gmail.com>
2025-03-26 21:28:48 +0300

    Sort: add 'comb sort' algorithm

--- my_sort.h
+++ my_sort.h
@@ -19,11 +19,12 @@ public:
 
     inline void swap(int& a, int& b);
 
+    // # Сортировки обменом
     double stupid(std::vector<int>& nums);
     double bubble(std::vector<int>& nums);
     double cocktail(std::vector<int>& nums);
     double oddEven(std::vector<int>& nums);
-    double oddEven_1(std::vector<int>& nums);
+    double comb(std::vector<int>& nums);
 
 
 };
