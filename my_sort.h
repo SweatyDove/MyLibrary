7c3a808a9ca078bee2cef572b9d79e7b3cbfd8c1
Alexey Perekhin <aperexin@gmail.com>
2025-04-11 20:23:03 +0300

    [quick sort]: refactoring

--- my_sort.h
+++ my_sort.h
@@ -26,13 +26,12 @@ public:
     double bubble(std::vector<int>& nums);
     double cocktail(std::vector<int>& nums);
     double oddEven(std::vector<int>& nums);
-    double oddEven_1(std::vector<int>& nums);
-    double oddEven_2(std::vector<int>& nums);
+    double oddEvenVer1(std::vector<int>& nums);
+    double oddEvenVer2(std::vector<int>& nums);
     double comb(std::vector<int>& nums);
     void quick(std::vector<int>& a, int start, int end);
-    void quick_1(std::vector<int>& a, int start, int end);
-    void quick_2(std::vector<int>& a, int start, int end);
-    void quick_3(std::vector<int>& a, int start, int end);
+    void quickClassic(std::vector<int>& a, int start, int end);
+
 
 
 
