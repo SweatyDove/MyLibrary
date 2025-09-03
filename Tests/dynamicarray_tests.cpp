dce5c8c0a5e3ee257757e87ca272820d1e8c7114
Alexey Perekhin <aperexin@gmail.com>
2025-09-04 00:09:23 +0300

    [STABLE]: add several tests for the <me::DynamicArray> class

--- Tests/dynamicarray_tests.cpp
+++ Tests/dynamicarray_tests.cpp
@@ -5,25 +5,85 @@
 
 
 
-TEST(GroupName, TestName)
+
+int main(int argc, char** argv)
 {
-//    my::DynamicArray<int> d {1, 2, 3};
-//    std::cout << d << std::endl;
+    ::testing::InitGoogleTest(&argc, argv);
+    //    ::testing::InitGoogleMock(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
+
+
+//==================================================================================================
+//          TYPE:   Test
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+//      COMMENTS:   ........
+//==================================================================================================
+TEST(TestDynamicArray, TestInsertionOperator)
+{
+    // # Arrange
+
+    my::DynamicArray<int> d {1, 2, 3};
+
+    ::testing::internal::CaptureStdout();
+    std::cout << d << std::endl;
 
 //    d.pushBack(4);
 //    d.pushBack(5);
 //    std::cout << d << std::endl;
 
-    ASSERT_TRUE(true);
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_STREQ(output.c_str(), "[1, 2, 3]\n");
 }
 
 
 
-
-int main(int argc, char** argv)
+//==================================================================================================
+//          TYPE:   Test
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+//      COMMENTS:   ........
+//==================================================================================================
+TEST(TestDynamicArray, TestPushBackFunc)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    ::testing::InitGoogleMock(&argc, argv);
+    // # Arrange
+    my::DynamicArray<int> d {12, 7, 34};
+    int size {d.size()};
 
-    return RUN_ALL_TESTS();
+    // # Act
+    d.pushBack(11);
+    size += 1;
+
+    // # Assert
+    EXPECT_EQ(size, 4);
+    EXPECT_EQ(d[size - 1], 11);
 }
+
+
+//==================================================================================================
+//          TYPE:   Test
+//   DESCRIPTION:   ........
+//    PARAMETERS:   ........
+//  RETURN VALUE:   ........
+//      COMMENTS:   ........
+//==================================================================================================
+TEST(TestDynamicArray, TestSubscriptionOperator)
+{
+    // # Arrange
+    my::DynamicArray<int> d {12, 7, 34};
+
+    // # Act
+    int value0 = d[0];
+
+    // # Assert
+    EXPECT_EQ(value0, 12);
+}
+
+
+
